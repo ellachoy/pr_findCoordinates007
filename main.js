bf4467395c0f1d02fddd729bcc3a1301d6173dd2
ella choy <egal21de@gmail.com>
2022-03-19 23:43:07 +0100

    fin_v2

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const horizontal= document.querySelector('.horizontal');
 const target= document.querySelector('.target');
 const tag= document.querySelector('.tag');
 
+addEventListener('load',()=>{
+    const targetRect = target.getBoundingClientRect();
+console.log(targetRect)
+const targetHalfWidth= targetRect.width / 2;
+const targetHalfHeight= targetRect.height / 2;
+
 document.addEventListener('mousemove',(event)=>{
     const x= event.clientX;
     const y= event.clientY;
@@ -10,12 +16,11 @@ document.addEventListener('mousemove',(event)=>{
 //   use translate to efficient code
    vertical.style.transform = ` translateX(${x}px)`;
   horizontal.style.transform = ` translateY(${y}px)`;
-    // vertical.style.left=`${x}px`;
-    // horizontal.style.top=`${y}px`;
 
-    target.style.top=`${y}px`;
-    target.style.left=`${x}px`;
-    tag.style.top=`${y}px`;
-    tag.style.left=`${x}px`;
+  target.style.transform = ` translate(${x-targetHalfWidth}px,${y-targetHalfHeight}px)`;
+
+  tag.style.transform = ` translate(${x-targetHalfWidth}px,${y-targetHalfHeight}px)`;
+
     tag.innerHTML=`${y}px, ${x}px`;
+})
 })
